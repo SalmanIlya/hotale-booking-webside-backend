c67b970e6f0e2713caa8281fa7d9f61d17f03890
Salamanilyas <salamanilyas101@gmail.com>
2023-02-23 20:16:27 +0500

    create route

--- routes/hotal.js
+++ routes/hotal.js
@@ -1,7 +1,12 @@
 const express=require("express")
+const hotal = require("../controllers/hotal")
 const app=express.Router()
 
-
+app.post("/create",hotal.createHotale)
+app.put("/:id",hotal.updateUser)
+app.get("/",hotal.getAll)
+app.get("/hotal/:id",hotal.getSingle)
+app.delete("/delete",hotal.deleteHotale)
 
 
 
