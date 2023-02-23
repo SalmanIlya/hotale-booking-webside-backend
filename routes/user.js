0c4f021e9df89bf9f1425076fd0eae84bcab4386
Salamanilyas <salamanilyas101@gmail.com>
2023-02-23 16:28:30 +0500

    user-routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const auth = require("../controllers/auth")
+const User = require("../controllers/User")
 const app=express.Router()
 
 
 app.post("/create",auth.CreateUser)
 app.post("/login",auth.LoginUser)
+app.put("/user/:id",User.updateUser)
+app.get("/:id",User.getOneUser)
+app.get("/",User.getAllUser)
+app.delete("/delete/:id",User.deleteUser)
 
 
 
