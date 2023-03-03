cacc714c0e7d83a91fcfdab15f706d6052aeb616
Salamanilyas <salamanilyas101@gmail.com>
2023-03-03 23:35:04 +0500

    create-jwt-token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const User=require("../models/User")
 const cryptojs=require("crypto-js")
 const dotenv=require("dotenv")
+const jwt=require("jsonwebtoken")
 dotenv.config({path:"config/config.env"})
 const secritkey=process.env.sk
+const jwtkey=process.env.jwttoken
 module.exports={
     // create user 
-    CreateUser:async(req,res)=>{
+    CreateUser:async(req,res,next)=>{
 const createUser=new User({
     username:req.body.username,
     email:req.body.email,
@@ -18,20 +20,25 @@ const createUser=new User({
 })
 try{
 const saveUser=await createUser.save()
+
 res.status(200).json(saveUser)
 }catch(err){
-res.status(500).json(err)
+next(err)
 }
     },
     // login user 
-    LoginUser:async(req,res)=>{
+    LoginUser:async(req,res,next)=>{
         try{
 const user=await User.findOne({username:req.body.username})
 const {password,isAdmin,...other}=user._doc
 if(user){
+   
     const pass=cryptojs.RC4.decrypt(user.password,secritkey).toString(cryptojs.enc.Utf8)
     if(pass===req.body.password){
-        res.status(202).json({...other})
+        const auth=jwt.sign({
+            user:{user}
+        },process.env.jwttoken)
+        res.status(202).json({...other,auth})
     }else{
         res.status(402).json("password does not match")
     }
@@ -39,7 +46,8 @@ if(user){
     res.status(404).json("user not found")
 }
         }catch(err){
-            res.status(500).json("error is :",err)
+          next(err)
+       
         }
     }
 }
